86d556eaed0cbad22d9c3bf2c2b1ed40f6b0f2a7
Tobias Berge <t.berge@shopware.com>
2023-07-20 14:12:03 +0200

    Fix devServer

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ A POC for an async PluginManager for the Shopware 6 Storefront.
 
 * `cd src/Storefront/Resources/app/storefront`
 * `npm install`
-* `npm run prod`
+* `npm run serve`
 
 Test page can be found here: `src/Storefront/Resources/app/storefront/index.html`
--- src/Storefront/Resources/app/storefront/webpack.config.js
+++ src/Storefront/Resources/app/storefront/webpack.config.js
@@ -1,6 +1,18 @@
 const path = require('path');
 
 module.exports = {
+    target: 'web',
+    mode: 'development',
+    devServer: {
+        open: 'google-chrome',
+        port: 9000,
+        contentBase: [
+            path.join(__dirname),
+            path.join(__dirname, 'dist')
+        ],
+        watchContentBase: true,
+        liveReload: true,
+    },
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
