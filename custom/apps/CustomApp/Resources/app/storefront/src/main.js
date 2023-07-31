1b382aca9d13fe0c42a17e3572c396bf009ffaca
Tobias Berge <t.berge@shopware.com>
2023-07-31 17:05:11 +0200

    Add custom app config

---
+++ custom/apps/CustomApp/Resources/app/storefront/src/main.js
@@ -0,0 +1,1 @@
+console.log('CustomApp - main.js');
--- src/Storefront/Resources/app/storefront/webpack.config.js
+++ src/Storefront/Resources/app/storefront/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = {
+const coreConfig = {
     target: 'web',
     mode: 'production',
     devServer: {
@@ -28,3 +28,34 @@ module.exports = {
         },
     },
 };
+
+appConfig = {
+    target: 'web',
+    mode: 'production',
+    devServer: {
+        open: 'google-chrome',
+        port: 9009,
+        contentBase: [
+            path.join(__dirname),
+            path.join(__dirname, 'dist')
+        ],
+        watchContentBase: true,
+        liveReload: true,
+    },
+    entry: path.resolve('./../../../../..', 'custom/apps/CustomApp/Resources/app/storefront/src/main.js'),
+    output: {
+        path: path.resolve('./../../../../..', 'custom/apps/CustomApp/Resources/app/storefront/dist'),
+        filename: 'custom-app.js',
+        chunkFilename: '[name].js',
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+        ],
+        alias: {
+            src: path.resolve(__dirname, 'src'),
+        },
+    },
+}
+
+module.exports = [coreConfig, appConfig];
