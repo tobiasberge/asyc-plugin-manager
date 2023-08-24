e5f8b6933198e66dc8542a137a3d96d846ab26d2
Tobias Berge <t.berge@shopware.com>
2023-08-24 14:05:11 +0200

    Skip three for debugging

--- custom/apps/ThreeJsApp/Resources/app/storefront/src/plugin/three-viewer.js
+++ custom/apps/ThreeJsApp/Resources/app/storefront/src/plugin/three-viewer.js
@@ -29,32 +29,32 @@ export default class Three extends window.PluginBaseClass {
     }
 
     async onViewerButtonClick(event) {
-        const THREE = await import(/* webpackChunkName: "three-js-vendor" */ 'three');
-
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, this.el.offsetWidth / this.el.offsetHeight, 0.1, 1000 );
-
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize( this.el.offsetWidth, this.el.offsetHeight );
-
-        this.el.querySelector('img').replaceWith(renderer.domElement);
-
-        const geometry = new THREE.BoxGeometry(2, 2, 2);
-        const material = new THREE.MeshBasicMaterial( { color: 0xff00000 } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add(cube);
-
-        camera.position.z = 5;
-
-        function animate() {
-            requestAnimationFrame( animate );
-
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
-
-            renderer.render( scene, camera );
-        }
-
-        animate();
+        // const THREE = await import(/* webpackChunkName: "three-js-vendor" */ 'three');
+        //
+        // const scene = new THREE.Scene();
+        // const camera = new THREE.PerspectiveCamera( 75, this.el.offsetWidth / this.el.offsetHeight, 0.1, 1000 );
+        //
+        // const renderer = new THREE.WebGLRenderer();
+        // renderer.setSize( this.el.offsetWidth, this.el.offsetHeight );
+        //
+        // this.el.querySelector('img').replaceWith(renderer.domElement);
+        //
+        // const geometry = new THREE.BoxGeometry(2, 2, 2);
+        // const material = new THREE.MeshBasicMaterial( { color: 0xff00000 } );
+        // const cube = new THREE.Mesh( geometry, material );
+        // scene.add(cube);
+        //
+        // camera.position.z = 5;
+        //
+        // function animate() {
+        //     requestAnimationFrame( animate );
+        //
+        //     cube.rotation.x += 0.01;
+        //     cube.rotation.y += 0.01;
+        //
+        //     renderer.render( scene, camera );
+        // }
+        //
+        // animate();
     }
 }
